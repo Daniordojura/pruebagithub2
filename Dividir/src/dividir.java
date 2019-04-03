e5cb767dbeab7f23d335daa6bf055dff44ae98fb
Daniel <Daniel@192.168.1.190>
2019-04-03 17:47:16 +0200

    Version 1 Formula para dividir

---
+++ Dividir/src/dividir.java
@@ -0,0 +1,16 @@
+
+public class dividir {
+	int num1;
+	int num2;
+	
+public dividir(int n1, int n2) {
+		num1 = n1;
+		num2 = n2;
+	}
+	
+	public int dividir() {
+		int resultado = num1 / num2;
+		return resultado;
+}
+}
+
